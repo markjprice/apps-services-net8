16ee1c297c2da060fab9697fba563ff5a403c697
Mark J Price <markjprice@gmail.com>
2023-05-17 12:26:15 +0100

    Add VSCode extension scripts

---
+++ vscode/scripts/install-vs-code-extensions.ps1
@@ -0,0 +1,7 @@
+code --install-extension ms-dotnettools.csharp
+code --install-extension tintoy.msbuild-project-tools
+code --install-extension ms-mssql.mssql
+code --install-extension humao.rest-client
+code --install-extension icsharpcode.ilspy-vscode
+code --install-extension zxh404.vscode-proto3
+code --install-extension ms-azuretools.vscode-azurefunctions
